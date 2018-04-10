50139545b22537793e126688908d8cdf15d7961f
Kishwar Kumar <kumar.kishwar@gmail.com>
2018-04-10 11:44:56 +0200

    Update style-transfer-video-main.py

--- vgg19-style-transfer-video/style-transfer-video-main.py
+++ vgg19-style-transfer-video/style-transfer-video-main.py
@@ -1,6 +1,4 @@
 # These are my hobby project codes developed in python using OpenCV and TensorFlow
-# Some of the projects are tested on Mac, Some on Raspberry Pi
-# Anyone can use these codes without any permission
 #
 # Contact info: Kishwar Kumar [kumar.kishwar@gmail.com]
 # Country: Germany
