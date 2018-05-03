3cfe3b2ccd869ae2107c1502235c083fb61df4e0
Kishwar Kumar <kumar.kishwar@gmail.com>
2018-05-04 00:00:16 +0200

    Updates
    Updates

--- rnn-voice-control-car/keras/gen_noise_data.py
+++ rnn-voice-control-car/keras/gen_noise_data.py
@@ -12,67 +12,30 @@ APA-style citation: "Warden P. Speech Commands: A public dataset for single-word
 BibTeX @article{speechcommands, title={Speech Commands: A public dataset for single-word speech recognition.}, author={Warden, Pete}, journal={Dataset available from http://download.tensorflow.org/data/speech_commands_v0.01.tar.gz}, year={2017} }
 '''
 
-fileIdx = 0
-
-# instantiate
-config = ConfigParser()
-
-# parse existing file
-config.read('config.ini')
-
-root = config.get('section_path', 'data_path')
-data = config.get('section_path', 'training_data')
-
-inPath = root + config.get('section_path', 'in_noise_path')
-outPath = data + config.get('section_path', 'out_noise_path')
-
-if not os.path.exists(outPath):
-    os.makedirs(outPath)
-
-# from doing_the_dishes.wav
-noise = AudioSegment.from_wav(inPath + r"doing_the_dishes.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
+def generate_noise_files():
+    fileIdx = 0
     
-# from dude_miaowing.wav
-noise = AudioSegment.from_wav(inPath + r"dude_miaowing.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
-
-# from exercise_bike.wav
-noise = AudioSegment.from_wav(inPath + r"exercise_bike.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
+    outAudioLen = 10 * 1000
     
-# from pink_noise.wav
-noise = AudioSegment.from_wav(inPath + r"pink_noise.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
+    # instantiate
+    config = ConfigParser()
     
-# from running_tap.wav
-noise = AudioSegment.from_wav(inPath + r"running_tap.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
+    # parse existing file
+    config.read('config.ini')
     
-# from white_noise.wav
-noise = AudioSegment.from_wav(inPath + r"white_noise.wav")
-ln = len(noise)
-for i in range(int(ln/3000)):
-    gnoise = noise[(i*3000):((i+1)*3000)]
-    gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
-    fileIdx += 1
+    root = config.get('section_path', 'data_path')
+    data = config.get('section_path', 'training_data')
+    
+    inPath = root + config.get('section_path', 'in_noise_path')
+    outPath = data + config.get('section_path', 'out_noise_path')
+    
+    if not os.path.exists(outPath):
+        os.makedirs(outPath)
+        
+    for file in os.listdir(inPath):
+        noise = AudioSegment.from_wav(inPath + file)
+        ln = len(noise)
+        for i in range(int(ln/outAudioLen)):
+            gnoise = noise[(i*outAudioLen):((i+1)*outAudioLen)]
+            gnoise.export(outPath + "gnoise-" + str(fileIdx) + ".wav", format="wav")
+            fileIdx += 1
