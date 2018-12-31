cb4f832798f5b89f9c73c2df3c14b03b43db83d4
Kishwar Kumar <kumar.kishwar@gmail.com>
2018-12-31 12:29:52 +0100

    Updates
    Updates

---
+++ tiny_yolo_tf_keras/yolo_utils.py
@@ -0,0 +1,175 @@
+import colorsys
+import imghdr
+import os
+import random
+from keras import backend as K
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+def read_classes(classes_path):
+    with open(classes_path) as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names
+
+def read_anchors(anchors_path):
+    with open(anchors_path) as f:
+        anchors = f.readline()
+        anchors = [float(x) for x in anchors.split(',')]
+        anchors = np.array(anchors).reshape(-1, 2)
+    return anchors
+
+def generate_colors(class_names):
+    hsv_tuples = [(x / len(class_names), 1., 1.) for x in range(len(class_names))]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+    random.seed(10101)  # Fixed seed for consistent colors across runs.
+    random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
+    random.seed(None)  # Reset seed to default.
+    return colors
+
+def scale_boxes(boxes, image_shape):
+    """ Scales the predicted boxes in order to be drawable on the image"""
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+    return boxes
+
+def preprocess_image(img_path, model_image_size):
+    image_type = imghdr.what(img_path)
+    image = Image.open(img_path)
+    resized_image = image.resize(tuple(reversed(model_image_size)), Image.BICUBIC)
+    image_data = np.array(resized_image, dtype='float32')
+    image_data /= 255.
+    image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+    return image, image_data
+
+def draw_boxes(image, boxes, labels):
+    image_h, image_w, image_ch = image.shape
+
+    for box in boxes:
+        xmin = int(box.xmin * image_w)
+        ymin = int(box.ymin * image_h)
+        xmax = int(box.xmax * image_w)
+        ymax = int(box.ymax * image_h)
+
+        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (0,255,0), 3)
+        cv2.putText(image, 
+                    labels[box.get_label()] + ' ' + str(box.get_score()), 
+                    (xmin, ymin - 13), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    1e-3 * image_h, 
+                    (0,255,0), 2)
+        
+    return image 
+        
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        
+        self.c     = c
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+        
+        return self.label
+    
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+            
+        return self.score
+
+def bbox_iou(box1, box2):
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    xi1 = np.max([box1.xmin, box2.xmin])
+    yi1 = np.max([box1.ymin, box2.ymin])
+    xi2 = np.min([box1.xmax, box2.xmax])
+    yi2 = np.min([box1.ymax, box2.ymax])
+    inter_area = (yi2 - yi1) * (xi2 - xi1)
+
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    box1_area = (box1.ymax - box1.ymin) * (box1.xmax - box1.xmin)
+    box2_area = (box2.ymax - box2.ymin) * (box2.xmax - box2.xmin)
+    union_area = box1_area + box2_area - inter_area
+    
+    # compute the IoU
+    iou = inter_area / union_area
+
+    return iou
+    
+def decode_netout(netout, anchors, nb_class, obj_threshold=0.3, nms_threshold=0.3):
+    grid_h, grid_w, nb_box = netout.shape[:3]
+
+    boxes = []
+    
+    # decode the output by the network
+    netout[..., 4]  = _sigmoid(netout[..., 4])
+    netout[..., 5:] = netout[..., 4][..., np.newaxis] * _softmax(netout[..., 5:])
+    netout[..., 5:] *= netout[..., 5:] > obj_threshold
+    
+    for row in range(grid_h):
+        for col in range(grid_w):
+            for b in range(nb_box):
+                # from 4th element onwards are confidence and class classes
+                classes = netout[row,col,b,5:]
+                
+                if np.sum(classes) > 0:
+                    # first 4 elements are x, y, w, and h
+                    x, y, w, h = netout[row,col,b,:4]
+
+                    x = (col + _sigmoid(x)) / grid_w # center position, unit: image width
+                    y = (row + _sigmoid(y)) / grid_h # center position, unit: image height
+                    w = anchors[2 * b + 0] * np.exp(w) / grid_w # unit: image width
+                    h = anchors[2 * b + 1] * np.exp(h) / grid_h # unit: image height
+                    confidence = netout[row,col,b,4]
+                    
+                    box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, confidence, classes)
+                    
+                    boxes.append(box)
+
+    # suppress non-maximal boxes
+    for c in range(nb_class):
+        sorted_indices = list(reversed(np.argsort([box.classes[c] for box in boxes])))
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+            
+            if boxes[index_i].classes[c] == 0: 
+                continue
+            else:
+                for j in range(i+1, len(sorted_indices)):
+                    index_j = sorted_indices[j]
+                    
+                    if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_threshold:
+                        boxes[index_j].classes[c] = 0
+                        
+    # remove the boxes which are less likely than a obj_threshold
+    boxes = [box for box in boxes if box.get_score() > obj_threshold]
+    
+    return boxes
+    
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def _softmax(x, axis=-1, t=-100.):
+    x = x - np.max(x)
+    
+    if np.min(x) < t:
+        x = x/np.min(x)*t
+        
+    e_x = np.exp(x)
+    
+    return e_x / e_x.sum(axis, keepdims=True)
